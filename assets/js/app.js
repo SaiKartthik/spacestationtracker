082ae3768ba0d1a4d062ad5ea588ea2d7086c8e6
SaiKartthik <saikarthikmedepalli@gmail.com>
2022-06-04 02:08:24 +0530

    0

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+var map = L.map('map').setView([0, 0], 2);
+var myIcon = L.icon({
+    iconUrl: 'https://www.nicepng.com/png/detail/62-627547_international-space-station-transparent-background-international-space-station.png',
+    iconSize: [38, 95],
+    
+});
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+const mark  = L.marker([0, 0], {icon : myIcon}).addTo(map) 
+const fSSD = async() => {
+    const res = await fetch("https://api.wheretheiss.at/v1/satellites/25544")
+    const data = await res.json()
+    lats = data.latitude
+    long = data.longitude
+    console.log(lats, long);
+    mark.setLatLng([lats, long])
+    
+    // .openPopup();
+}
+
+setInterval(fSSD,1000)
